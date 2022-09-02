83fd65a16b83111378d706489c4833eb26d1d2ce
Donald Steve Kapyouo Tchuisseu <dkapyouo@gmail.com>
2022-09-02 09:45:20 +0000

    module 1

--- main.py
+++ main.py
@@ -4,10 +4,21 @@ x = 3
 y = "Kapygenius"
 z = True
 
+#nom = input("comment tu t'appeles ?")
+age = int(input("tu as quel age ?"))
+
+#print(f"tu t'appeles: {nom} tu as: {age} ans")
+#print("tu t'appeles:" + nom + " tu as: " + str(age) + " ans")
+
+print(age * 5)
+
+#print(type(nom))
+print(type(age))
+
 #x = x ** 5
 
 #print(type(x))
-print(x != z)
+#print(x != z)
 
 #X = 7
 #y = "Kapylearn"
