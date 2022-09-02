4c2c4b377951066d3df47e4c0b3d4e4197f361ab
Donald Steve Kapyouo Tchuisseu <dkapyouo@gmail.com>
2022-09-02 19:49:14 +0000

    solution

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello")
+weight = int(input("What is your weight? "))
+height = float (input("What is your height? "))
+
+bmi = weight / (height**2)
+bmi = int(bmi)
+
+print(f"Ton BMI est {bmi}.")
+print(f"your weight is {weight} kg and your height is {height}m.")
