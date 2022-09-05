7a6389a15ea0abd03374eed53f1c2879792aa95d
Donald Steve Kapyouo Tchuisseu <dkapyouo@gmail.com>
2022-09-05 17:14:34 +0000

    les fonctions

--- main.py
+++ main.py
@@ -1,30 +1,66 @@
-i = 0
-while i < 6:
-  print(f"{i}) hello")
-  if i == 3:
-    break
-  i += 1 #i=i+1
+print("hello")
 
-fruits = ["orange", "ananas", "mangue"]
-dicPerson = {"nom":"donald", "prenom":"ange"}
+def myFunction(age, nom, sexe = "m"):
+  print(f"hello {nom} from function {age} et ton sexe {sexe}")
 
-#fruits = tuple(fruits)
+def myFunc2(foods):
+  for x in foods:
+    print(x)
 
-for x in fruits:
-  if x == "ananas":
-    continue
-  for y in x:
-    print(y)
-  print("")
-  
+myFunction(nom = "donald", age = 16, sexe="f")
+myFunction(14, "ange", "f")
 
-for cle, valeur in dicPerson.items():
-  print(f"{cle} - {valeur}")
+#myFunc2(["banane", "Ero", "Orange"])
 
-nom = "donald"
+# f(n) = f(n-1) + n, f(0)=1
 
-for letter in nom:
-  print(letter)
+def f(n):
+  if n == 0:
+    return 1
+  else:
+    return f(n-1) + n
 
-for x in range(6):
-  print(f"{x}) hello")
+resultat = f(3)
+
+print(resultat)
+
+carre = lambda x, y : x**y
+
+print(carre(5, 3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#list comprehension
+myList = [1, 3, 4, 5, 6]
+
+#myListCarre = []
+#for x in myList:
+#  if x % 2 == 1:
+#    myListCarre.append(x**2)
+
+myListCarre = [ x**2 for x in myList if x % 2 == 1 ]
+
+#print(myListCarre)
